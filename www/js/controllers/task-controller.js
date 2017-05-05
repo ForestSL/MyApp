c6a6b8cae7c920754963c1ff682958083c768c32
Resudualgave <15651701676@163.com>
2017-05-05 20:14:22 +0800

    a little update

--- www/js/controllers/task-controller.js
+++ www/js/controllers/task-controller.js
@@ -9,4 +9,24 @@ angular.module('task-controller',[])
         	$state.go("tab.list");
     	};
 
+
+		$scope.tabIndex = '任务申请状态';
+
+        $scope.isOne = false;
+        $scope.isTwo = false;
+
+        $scope.showTab = function(tabIndex){
+            if(tabIndex=='任务申请状态'){
+                $scope.isOne = true;
+                $scope.isTwo = false;
+            }else{
+                $scope.isHaveMoreData = true;
+                $scope.isOne = false;
+                $scope.isTwo = true;
+            }
+            $scope.tabIndex = tabIndex;
+        };
+
+        $scope.showTab($scope.tabIndex);
+
 		}]);
